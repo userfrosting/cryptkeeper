f12a8497f1add2a6dbd01649d6cb6485b9980f68
alexweissman <alex@alexanderweissman.com>
2017-12-30 00:03:58 -0500

    pull USD and BTC quotes from CMC

--- src/Sprunje/CurrencySprunje.php
+++ src/Sprunje/CurrencySprunje.php
@@ -8,6 +8,7 @@
 namespace UserFrosting\Sprinkle\Cryptkeeper\Sprunje;
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+use GuzzleHttp\Client as Guzzle;
 use UserFrosting\Sprinkle\Core\Sprunje\Sprunje;
 use UserFrosting\Sprinkle\Core\Facades\Debug;
 use UserFrosting\Sprinkle\Cryptkeeper\Database\Models\Currency;
@@ -45,5 +46,31 @@ class CurrencySprunje extends Sprunje
         $query = (new Currency())->newQuery();
 
         return $query;
+    }
+
+    protected function applyTransformations($collection)
+    {
+        // Retrieve coinmarketcap quotes and map by CMC id
+        $client = new Guzzle;
+        $response = $client->request('GET', 'https://api.coinmarketcap.com/v1/ticker');
+        $quotes = collect(json_decode($response->getBody(), true));
+
+        $quotesKeyed = $quotes->mapWithKeys(function ($item) {
+            return [$item['id'] => $item];
+        });
+
+        // Map fiat and BTC prices to currencies in database
+        $collection->transform(function ($item, $key) use ($quotesKeyed) {
+            $name = $item->name_cmc;
+            $quote = isset($quotesKeyed[$name]) ? $quotesKeyed[$name] : null;
+            if ($quote) {
+                $item->price_usd = $quote['price_usd'];
+                $item->price_btc = $quote['price_btc'];
+            }
+
+            return $item;
+        });
+
+        return $collection;
     }
 }
